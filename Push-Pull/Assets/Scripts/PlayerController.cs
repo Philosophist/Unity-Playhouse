aae0e5e887715d31348f1bed47dfc2619666690b
Steve The Philosophist <srp2120@gmail.com>
2018-10-05 16:13:52 -0700

    Reorganizing

---
+++ Push-Pull/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+    }
+
+}
