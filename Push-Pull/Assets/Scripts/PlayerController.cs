d3c90e89391c2d4bc87f2791daf661b1e76f2a5c
Steve The Philosophist <srp2120@gmail.com>
2018-10-05 22:19:54 -0700

    Steve's Sandbox Update

--- Push-Pull/Assets/Scripts/PlayerController.cs
+++ Push-Pull/Assets/Scripts/PlayerController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    private void FixedUpdate()
+    private Rigidbody2D rb2d;
+    public float speed = 0.5f;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxisRaw("Horizontal");
         float moveVertical = Input.GetAxisRaw("Vertical");
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        Vector2 movement = new Vector2(moveHorizontal * speed, moveVertical * speed);
+        rb2d.AddForce(movement);
     }
 
 }
